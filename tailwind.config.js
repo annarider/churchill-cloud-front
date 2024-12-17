67363eefb7933ff8d42774fec3a3553d10e22f35
Anna Rider <3461836+annarider@users.noreply.github.com>
2024-12-17 11:12:01 -0700

    Add brand colors following tailwind styling
    Following Tailwind conventions for adding style
    colors to tailwind config. Still need to confirm
    with Victor on color palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./_includes/**/*.{html,md}",
-    "./_layouts/**/*.{html,md}",
-    "./_pages/**/*.{html,md}",
-    "./*.{html,md}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#00AEEF", // aqua
+          DEFAULT: "#005CAB", // medium blue
+          dark: "#000437", // dark navy
+        },
+        secondary: {
+          light: "#FFF", // white
+          DEFAULT: "#FBB040", // orange yellow
+          dark: "#EF4137", // red orange
+        },
+        accent: {
+          light: "#39B54A", // green
+          DEFAULT: "#00B0A3", // teal
+          dark: "#00366F", // dark blue
+        },
+        neutral: "#414042", // charcoal
+      },
+    },
   },
   plugins: [],
 };
